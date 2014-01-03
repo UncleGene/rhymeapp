90140f2c5d37cffdb70b3806cce65c938fb25c9a
Eugene Kalenkovich <rubify@softover.com>
2014-01-02 16:53:49 -0800

    Update code for newer gem version

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require 'rhymes'
 require 'json'
 require 'haml'
 
-Rhymes.setup( raw_dict: File.dirname(__FILE__) + '/data/cmudict.0.7a', compiled: File.dirname(__FILE__) + '/data/rhymes.dat')
-
 set :views, File.dirname(__FILE__)
 
 helpers do 
